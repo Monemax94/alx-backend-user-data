3adc542b511c2374f477f2973e9be819b152a67f
Monemax94 <mondayodoh94gmail.com>
2023-05-03 00:21:24 +0100

    The Method that returns the decoded value of a Base64 string

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,8 +2,8 @@
 """Basic authentication module for the API.
 """
 import re
-# import base64
-# import binascii
+import base64
+import binascii
 # from typing import Tuple, TypeVar
 
 from .auth import Auth
@@ -26,21 +26,21 @@ class BasicAuth(Auth):
                 return field_match.group('token')
         return None
 
-#     def decode_base64_authorization_header(
-#             self,
-#             base64_authorization_header: str,
-#             ) -> str:
-#         """Decodes a base64-encoded authorization header.
-#         """
-#         if type(base64_authorization_header) == str:
-#             try:
-#                 res = base64.b64decode(
-#                     base64_authorization_header,
-#                     validate=True,
-#                 )
-#                 return res.decode('utf-8')
-#             except (binascii.Error, UnicodeDecodeError):
-#                 return None
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str,
+            ) -> str:
+        """Decodes a base64-encoded authorization header.
+        """
+        if type(base64_authorization_header) == str:
+            try:
+                res = base64.b64decode(
+                    base64_authorization_header,
+                    validate=True,
+                )
+                return res.decode('utf-8')
+            except (binascii.Error, UnicodeDecodeError):
+                return None
 
 #     def extract_user_credentials(
 #             self,
