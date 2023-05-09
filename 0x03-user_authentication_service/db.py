a9c5b99e0a9f09ec2c79219cf8d55cef92d75c5b
Monemax94 <mondayodoh94gmail.com>
2023-05-09 10:30:33 +0100

    Implementing the add_user method in DB class that adds a new user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -30,11 +30,10 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-
     def add_user(self, email: str, hashed_password: str) -> User:
         """Adds a user to the db
         """
-         try:
+        try:
             new_user = User(email=email, hashed_password=hashed_password)
             self._session.add(new_user)
             self._session.commit()
