ee027e92f4d97760efa892ff305a7ae505f4ea9a
Monemax94 <mondayodoh94gmail.com>
2023-05-11 12:29:25 +0100

    implementing a _generate_uuid function in the auth module.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,7 +2,7 @@
 """A module for authentication-related routines.
 """
 import bcrypt
-# from uuid import uuid4
+from uuid import uuid4
 from typing import Union
 from sqlalchemy.orm.exc import NoResultFound
 
