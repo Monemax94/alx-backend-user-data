d129501d3d1339d2a8e96e96c2faf8616fa8c509
Monemax94 <mondayodoh94gmail.com>
2023-05-11 07:11:29 +0100

    pycodestyle

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -44,7 +44,6 @@ class DB:
             new_user = None
         return new_user
 
-
     def find_user_by(self, **kwargs) -> User:
         """Finds a user based on a set of filters.
         """
@@ -61,4 +60,3 @@ class DB:
         if result is None:
             raise NoResultFound()
         return result
-
