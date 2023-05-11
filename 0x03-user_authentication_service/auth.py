8f61f4887b62d1065b5e07f2ca539e8d03d8860d
Monemax94 <mondayodoh94gmail.com>
2023-05-11 07:34:56 +0100

    pycodestyle

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,12 +2,12 @@
 """A module for authentication-related routines.
 """
 import bcrypt
-#from uuid import uuid4
-#from typing import Union
-#from sqlalchemy.orm.exc import NoResultFound
+# from uuid import uuid4
+# from typing import Union
+# from sqlalchemy.orm.exc import NoResultFound
 
-#from db import DB
-#from user import User
+# from db import DB
+# from user import User
 
 
 def _hash_password(password: str) -> bytes:
